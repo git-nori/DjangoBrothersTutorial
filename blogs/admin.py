0ed927b1ba96d2d49fbc4cb434cf1152ae0a111f
HironoriSato <maza.hs5360@gmail.com>
2019-07-08 23:20:04 +0900

    [mod]adminページの設定を追加

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Blog
 
 # Register your models here.
+
+# admin.site.register(Blog)  # BlogモデルをAdminページで利用できるよう設定
+
+# Adminページに複数のフィールドを表示させるよう設定
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_datetime', 'updated_datetime')  # 指定したフィールドが管理ページに表示されるよう設定
+    list_display_links = ('id', 'title')  # 指定したフィールドはリンクがつくよう設定
+
+
+admin.site.register(Blog, BlogAdmin)
