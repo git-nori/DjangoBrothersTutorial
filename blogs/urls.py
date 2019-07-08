74694d6d3d50927f0a74e8b8558f252c9f09243a
HironoriSato <maza.hs5360@gmail.com>
2019-07-09 00:00:10 +0900

    [mod]詳細ページを新規追加

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'blogs'
 urlpatterns = [
     path('', views.index, name='index'),  # http://127.0.0.1:8000/へのアクセスに対してviews.indexメソッドを呼ぶ
+    path('detail/<int:blog_id>/', views.detail, name='detail'),  # blog_idはviews.pyにも渡される
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -5,3 +5,8 @@ from .models import Blog
 def index(request):
     blogs = Blog.objects.order_by('-created_datetime')  # クエリセットの取得
     return render(request, 'blogs/index.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):  # blogs/urls.pyに指定したblog_idを受け取る
+    blog = Blog.objects.get(id=blog_id)
+    return render(request, 'blogs/detail.html', {'blog': blog})
