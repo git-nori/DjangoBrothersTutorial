58fa96c1a624551cc936f7f7e069823814066d6b
HironoriSato <maza.hs5360@gmail.com>
2019-07-08 23:04:33 +0900

    [add]Blogモデルの新規作成

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# Pythonコードでデータベースの構造を記述し、簡単に操作できるようにmodelsを設定
+class Blog(models.Model):
+    title = models.CharField(blank=False, null=False, max_length=150)
+    text = models.TextField(blank=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """オブジェクトを扱う上で使用するフィールドを指定
+            ブログオブジェクトの一覧ページ（http://127.0.0.1:8000/admin/blogs/blog/）にいくと、それぞれのブログのタイトルが表示される
+        """
+        return self.title
